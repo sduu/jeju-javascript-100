9af894886aec7799e66ead2816f079bb73394b9d
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-09-30 21:55:23 +0900

    Solve : 043~050 문제 풀이

---
+++ part1/44.js
@@ -0,0 +1,41 @@
+/* # 문제44 : 각 자리수의 합
+
+**사용자가 입력한 양의 정수의 각 자리수의 합을 구하는 프로그램**을 만들어주세요
+
+**예를들어**
+18234 = 1+8+2+3+4 이고 정답은 18 입니다.
+3849 = 3+8+4+9 이고 정답은 24입니다.
+
+```jsx
+**입출력**
+
+입력 : 18234
+출력 : 18
+
+입력 : 3849
+출력 : 24
+``` */
+
+const num = prompt('숫자를 입력하세요').split('').map(Number);
+
+function sum(num) {
+    const answer = [...num].reduce((acc, cur) => {
+        return acc + cur;
+    }, 0);
+
+    return answer;
+}
+
+console.log(sum(num));
+
+/* 답안 : 
+let n = prompt('숫자를 입력하세요.');
+let sum = 0;
+
+while(n !== 0){
+  sum += (n % 10);
+  n = Math.floor(n/10);
+}
+
+console.log(sum);
+ */

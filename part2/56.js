c833f9b7d55581694c1d7db90a059452097e3c51
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-10-03 22:42:04 +0900

    Solve : 055~057 문제 풀이

---
+++ part2/56.js
@@ -0,0 +1,72 @@
+// 문제56 : 객체의 함수 응용
+
+// 다음의 객체가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력하세요.
+
+// ```jsx
+// **데이터**
+// nationWidth = {
+//      'korea': 220877,
+//      'Rusia': 17098242,
+//      'China': 9596961,
+//      'France': 543965,
+//      'Japan': 377915,
+//      'England' : 242900,
+// }
+
+// **출력**
+// England 22023
+// ```
+
+const nationWidth = {
+    korea: 220877,
+    Rusia: 17098242,
+    China: 9596961,
+    France: 543965,
+    Japan: 377915,
+    England: 242900,
+};
+
+const targetWidth = nationWidth['korea'];
+delete nationWidth['korea'];
+
+const answer = Object.entries(nationWidth).reduce((acc, cur) => {
+    const gap = Math.abs(cur[1] - targetWidth);
+    if (gap < acc[1]) {
+        return [cur[0], gap];
+    }
+});
+
+console.log(`${answer[0]} ${answer[1]}`);
+
+/* 
+답안 
+
+const nationWidth = {
+	'korea': 220877,
+	'Rusia': 17098242,
+	'China': 9596961,
+	'France': 543965,
+	'Japan': 377915,
+	'England' : 242900,
+};
+
+const w = nationWidth['korea'];
+
+delete nationWidth['korea'];
+
+const entry = Object.entries(nationWidth);
+const values = Object.values(nationWidth);
+
+//gap에 최댓값 저장
+let gap = Math.max.apply(null, values);
+let item = [];
+
+for (let i in entry){
+  if (gap > Math.abs(entry[i][1] - w)){
+    gap = Math.abs(entry[i][1] - w);
+    item = entry[i];
+  }
+}
+
+console.log(item[0], item[1] - w);
+*/

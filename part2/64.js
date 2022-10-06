37bfd040735e3e2ec0b64f344b19eb9b614a3161
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-10-06 23:30:17 +0900

    Solve : 064~066 문제 풀이

---
+++ part2/64.js
@@ -0,0 +1,49 @@
+/* 문제64 : 이상한 엘레베이터
+
+정량 N에 정확히 맞춰야만 움직이는 화물용 엘리베이터가 있습니다.
+화물은 7kg, 3kg 두 가지이며 팔이 아픈 은후는 가장 적게 화물을 옮기고 싶습니다.
+
+예를 들어 정량이 24kg이라면 3kg 8개를 옮기는 것보다는
+7kg 3개, 3kg 1개 즉 4개로 더 적게 옮길 수 있습니다.
+
+**입력**
+정량 N이 입력됩니다.
+
+**출력**
+가장 적게 옮길 수 있는 횟수를 출력합니다.
+만약 어떻게 해도 정량이 N이 되지 않는다면 -1을 출력합니다. */
+
+const limit = [7, 3];
+let input = parseInt(prompt('숫자를 입력하세요'));
+let answer = 0;
+
+while (limit.length > 0) {
+    const num = limit.shift();
+    if (parseInt(input / num) === 0) {
+        answer = 1;
+        break;
+    }
+    answer += parseInt(input / num);
+    input = parseInt(input % num);
+}
+
+console.log(answer);
+
+/* 답안
+
+let N = parseInt(prompt('정량을 입력하세요'), 10);
+let result = 0;
+
+while (true){
+  if (N%7 === 0){
+    result += parseInt(N/7, 10);
+    console.log(result);
+    break;
+  }
+  N -= 3;
+  result += 1;
+  if (N < 0){
+    console.log(-1);
+    break;
+  }
+} */
